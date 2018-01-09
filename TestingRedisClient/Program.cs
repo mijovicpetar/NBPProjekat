eb2f7b9753f50696343919f87917225bdd095a9f
Petar Mijoivc <mijovic.petar@elfak.rs>
2018-01-09 19:13:18 +0100

    Added RedisClientAPI and project for testing it.

---
+++ TestingRedisClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RedisCommunicator;
+
+namespace TestingRedisClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                CommunicatorRedis redis = new CommunicatorRedis("localhost");
+                redis.SetValue("kljuc", "vrednost");
+                var result = redis.GetValue("kljuc");
+                if (result != null)
+                    Console.WriteLine("Proso je.");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
